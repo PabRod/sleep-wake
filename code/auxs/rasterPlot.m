2146f8817a35b6264295ffdf357ea8c7a5439dff
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-10-05 12:44:29 +0200

    Encapsulated raster matrix generation

--- code/auxs/rasterPlot.m
+++ code/auxs/rasterPlot.m
@@ -3,15 +3,9 @@ function rasterPlot(ts, asleep)
 
 %% Reshape the asleep vector
 % To turn it into a raster matrix
-awake_fun = @(t) interp1(ts, 1.0.*(~asleep), t, 'nearest');
-
 nDays = floor(ts(end));
 nDailySamples = 1000;
-raster = NaN(nDays-1, nDailySamples);
-for i = 1:nDays-1
-    ts_eval = linspace(i-1, i+1, nDailySamples); % Evaluate two days per row
-    raster(i, :) = awake_fun(ts_eval);
-end
+raster = reshapeAsRaster(ts, asleep, nDailySamples); 
 
 %% Rescale the time vector with units
 ts_hours = linspace(0, 48, nDailySamples); % Each row covers two days
@@ -23,5 +17,22 @@ xlabel('h');
 ylabel('Day');
 xticks([0 6 12 18 24 30 36 42 48]);
 xticklabels({'0','6','12','18','0','6','12','18','0'});
+
+end
+
+function raster = reshapeAsRaster(ts, asleep, nDailySamples)
+%RASTER turns the asleep vector into a raster matrix
+
+%% Interpolate
+% Required for sampling at any t
+awake_fun = @(t) interp1(ts, 1.0.*(~asleep), t, 'nearest');
+
+%% Reshape
+nDays = floor(ts(end));
+raster = NaN(nDays-1, nDailySamples);
+for i = 1:nDays-1
+    ts_eval = linspace(i-1, i+1, nDailySamples); % Evaluate two days per row
+    raster(i, :) = awake_fun(ts_eval);
+end
 
 end
