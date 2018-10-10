695caab278726e4785dea2a18d617b1e76ef7b88
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-10-10 16:09:50 +0200

    Improved polToCartFlow
    Both in documentation and algorithm

--- code/auxs/polToCartFlow.m
+++ code/auxs/polToCartFlow.m
@@ -8,11 +8,42 @@ function dy_cart = polToCartFlow(dy_pol, varargin)
 %                     1 + r(1).^2];
 %
 %   dy_cart = polToCartFlow(dy_pol);
+%
+%   Rationale:
+%
+%   Using the chain rule for differentiation on the (r, theta) -> (x, y)
+%   transformation:
+%   x = r*cos(theta) => x' = cos(theta)*r'-r*sin(theta)*theta'
+%   y = r*sin(theta) => y' = sin(theta)*r'+r*cos(theta)*theta'
+%
+%   Equivalently, in matrix form:
+%   [x'; 
+%    y'] 
+%    = 
+%   [cos(theta), -r*sin(theta);
+%   [sin(theta), r*cos(theta)]
+%   *
+%   [r'(r, theta);
+%    theta'(r, theta)];
+%
+%   Lastly, rewriting everything as a function of (x, y):
+%
+%   [x'; 
+%    y'] 
+%    = 
+%   [x/sqrt(x^2+y^2), -y;
+%   [y/sqrt(x^2+y^2), x]
+%   *
+%   [r'(sqrt(x^2 + y^2), atan(y/x));
+%    theta'(sqrt(x^2 + y^2), atan(y/x))];
+%
+%   Pablo Rodríguez-Sánchez
+%   pabrod.github.io	
 
 %% Auxiliary functions
 % Transforms (x, y) -> (r, theta)
 radius = @(y) norm(y, 2);
-theta = @(y) atan(y(2)./y(1));
+theta = @(y) atan2(y(2), y(1));
 
 % Transformation's jacobian
 transform = @(y) [y(1)./radius(y),  -y(2);
