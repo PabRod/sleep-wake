c03bb1ddb22d66d0174c84711e82d5a30ae64ce8
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2018-10-16 17:13:09 +0200

    Improved strogatz_script

--- scripts/strogatz_script.m
+++ scripts/strogatz_script.m
@@ -3,13 +3,25 @@ close all;
 clear;
 clc;
 
-%% PR model
+%% Model
 % As described in in:
 % Strogatz, S. H. (1987). 
 % Human sleep and circadian rhythms: a simple model based on two coupled oscillators. 
 % Journal of Mathematical Biology, 25(3), 327–347. http://doi.org/10.1007/BF00276440
 pars = genPars('strogatz1987');
 pars.w2 = 0.835 / 24; % Transition at 0.84 / 24 h^-1
+
+%% Auxiliary variables
+omega = pars.w1 - pars.w2;
+C = pars.C1 + pars.C2;
+
+isSync = abs(omega/C) <= 1;
+if isSync
+    T_beat = NaN;
+else
+    T_beat = 1/sqrt(omega^2 - C^2) / 24; % Beat period in days
+    assert(isreal(T_beat));
+end
 
 %% Generate time series
 nDays = 60; % d
@@ -39,3 +51,7 @@ ax.GridLineStyle = '-';
 
 subplot(1, 2, 2);
 rasterPlot(ts_days, asleep);
+
+if ~isSync
+    title(sprintf('Raster plot \n Beat period: %0.2f days', T_beat));
+end
